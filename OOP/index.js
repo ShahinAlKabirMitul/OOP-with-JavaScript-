a12bbb4cb96c1e8526c5472f3d05441c8f073b54
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-12 22:59:39 +0600

    Factories

--- OOP/index.js
+++ OOP/index.js
@@ -1,11 +1,10 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function() {
-    console.log('Drow');
-  },
-};
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function() {
+      console.log('draw');
+    },
+  };
+}
+const circle=createCircle(1);
 circle.draw();
