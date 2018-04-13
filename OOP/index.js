ce811d3228d31f934aa103a5e4acd0223a73542a
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-13 14:34:19 +0600

    Private Properties and Methods

--- OOP/index.js
+++ OOP/index.js
@@ -1,17 +1,31 @@
 function Circle(radious) {
   this.radius = radious;
+  let defaultLocation = { x: 0, y: 0 };
+  let computeOptimumLocation = function(fator) {
+    //...
+  };
   this.draw = function() {
     console.log('drow');
+    computeOptimumLocation(1.1);
   };
 }
 const circle = new Circle(10);
+circle.draw();
 
-for (let key in circle) {
-  if (typeof circle[key] !== 'function') console.log(key, circle[key]);
-}
-const keys = Object.keys(circle);
-console.log(keys);
-if ('radius' in circle) console.log('Circle has a radius');
+// function Circle(radious) {
+//   this.radius = radious;
+//   this.draw = function() {
+//     console.log('drow');
+//   };
+// }
+// const circle = new Circle(10);
+
+// for (let key in circle) {
+//   if (typeof circle[key] !== 'function') console.log(key, circle[key]);
+// }
+// const keys = Object.keys(circle);
+// console.log(keys);
+// if ('radius' in circle) console.log('Circle has a radius');
 
 // let obj = { value: 10 };
 // function increaseObj(obj) {
