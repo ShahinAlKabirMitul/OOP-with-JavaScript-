020fd507df6fa33d58347ca19ab42198edda0a99
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-13 10:47:02 +0600

    Value VS Reference type

--- OOP/index.js
+++ OOP/index.js
@@ -1,21 +1,38 @@
-// Factory Function
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function() {
-      console.log('draw');
-    },
-  };
+let obj = { value: 10 };
+function increaseObj(obj) {
+  obj.value++;
 }
-const circle = createCircle(1);
+increaseObj(obj);
+console.log(obj);
 
-// Constractor Functions
-function Circle(radius) {
-  console.log('this', this);
-  (this.radius = radius),
-    (this.draw = function() {
-      console.log('Drow');
-    });
+let number = 10;
+function increase(number) {
+  number++;
 }
-const another = new Circle(1);
-another.draw();
+increase(10);
+
+let x = { value: 10 };
+let y = x;
+x.value = 20;
+
+// // Factory Function
+// function createCircle(radius) {
+//   return {
+//     radius,
+//     draw: function() {
+//       console.log('draw');
+//     },
+//   };
+// }
+// const circle = createCircle(1);
+
+// // Constractor Functions
+// function Circle(radius) {
+//   console.log('this', this);
+//   (this.radius = radius),
+//     (this.draw = function() {
+//       console.log('Drow');
+//     });
+// }
+// const another = new Circle(1);
+// another.draw();
