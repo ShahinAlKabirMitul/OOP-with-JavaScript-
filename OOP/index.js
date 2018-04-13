47f90ca45c81aee0f5055ec33321f53de13fe26f
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-13 10:56:28 +0600

    Adding or Removing Properties

--- OOP/index.js
+++ OOP/index.js
@@ -1,19 +1,31 @@
-let obj = { value: 10 };
-function increaseObj(obj) {
-  obj.value++;
+function Circle(radious) {
+  this.radious = radious;
+  this.draw = function() {
+    console.log('drow');
+  };
 }
-increaseObj(obj);
-console.log(obj);
+const circle = new Circle(1);
+circle.location = { x: 1, y: 2 };
+const propertryName = 'location';
+circle[propertryName] = { x: 1, y: 2 };
+delete circle['location'];
 
-let number = 10;
-function increase(number) {
-  number++;
-}
-increase(10);
+// let obj = { value: 10 };
+// function increaseObj(obj) {
+//   obj.value++;
+// }
+// increaseObj(obj);
+// console.log(obj);
 
-let x = { value: 10 };
-let y = x;
-x.value = 20;
+// let number = 10;
+// function increase(number) {
+//   number++;
+// }
+// increase(10);
+
+// let x = { value: 10 };
+// let y = x;
+// x.value = 20;
 
 // // Factory Function
 // function createCircle(radius) {
