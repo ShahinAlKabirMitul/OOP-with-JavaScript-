871c256d80e5906f7567d963d8f96498b1e768f2
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-15 00:00:05 +0600

    premative op

--- OOP/stopwatch.js
+++ OOP/stopwatch.js
@@ -3,34 +3,53 @@ function StopWatch() {
     endtime,
     running,
     duration = 0;
-  this.start = function() {
-    if (running) {
-      throw new Error('StopWatch all ready start');
-    }
-    running = true;
-    startime = new Date();
-    console.log('start time', startime);
-  };
-  this.stop = function() {
-    if (!running) {
-      throw new Error('StopWatch not stat yet , please start first');
-    }
-    running = false;
-    endtime = new Date();
-    const seconds = (endtime.getTime() - startime.getTime()) / 1000;
-    duration += seconds;
-  };
-  this.reset = function() {
-    startime = null;
-    endtime = null;
-    duration = 0;
-    running = false;
-  };
 
   Object.defineProperty(this, 'duration', {
     get: function() {
       return duration;
     },
+    set: function(value) {
+      duration = value;
+    },
+  });
+  Object.defineProperty(this, 'startime', {
+    get: function() {
+      return startime;
+    },
+  });
+  Object.defineProperty(this, 'endtime', {
+    get: function() {
+      return endtime;
+    },
+  });
+  Object.defineProperty(this, 'running', {
+    get: function() {
+      return running;
+    },
   });
 }
+StopWatch.prototype.start = function() {
+  if (running) {
+    throw new Error('StopWatch all ready start');
+  }
+  this.running = true;
+  this.startime = new Date();
+  console.log('start time', startime);
+};
+StopWatch.prototype.stop = function() {
+  if (!running) {
+    throw new Error('StopWatch not stat yet , please start first');
+  }
+  this.running = false;
+  this.endtime = new Date();
+  const seconds = (endtime.getTime() - startime.getTime()) / 1000;
+  this.duration += seconds;
+};
+StopWatch.prototype.reset = function() {
+  this.startime = null;
+  this.endtime = null;
+  this.duration = 0;
+  this.running = false;
+};
+
 const sw = new StopWatch();
