7d78d115d3b3cc157de649aebc6aa5e271a97ae8
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-16 07:57:29 +0600

    Solution- Prototypical Inheritance

---
+++ OOP/prototypeinheritance.js
@@ -0,0 +1,20 @@
+function HtmlElement() {
+  this.click = function() {
+    console.log('click');
+  };
+}
+HtmlElement.prototype.focus = function() {
+  console.log('fucas');
+};
+
+function HtmlSelectElement(items = []) {
+  this.items = items;
+  this.addItem = function(item) {
+    this.items.push(item);
+  };
+  this.removeItem = function(item) {
+    this.items.splice(this.items.indexOf(item), 1);
+  };
+}
+HtmlSelectElement.prototype = new HtmlElement();
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
